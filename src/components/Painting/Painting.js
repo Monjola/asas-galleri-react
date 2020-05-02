98655e0d984ec2a61d6b1671873d1e949296e889
Måns Nilsson <mans.n@hotmail.com>
2020-05-02 17:45:38 +0200

    added a picture

---
+++ src/components/Painting/Painting.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Painting() {
+    return (
+        <div>
+            <div><img src="img/tavla.jpg" alt="tavla1" /></div>
+            <p>Två tavlor</p>
+            <p>2000kr</p>
+        </div>
+    )
+}
