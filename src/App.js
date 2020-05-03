5ed103a8275096e1d4765f0818b30b95c50b6441
Måns Nilsson <mans.n@hotmail.com>
2020-05-03 14:47:45 +0200

    added pics, font. changed layout
    added some more pictures, changed font and changed structure

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
   return (
     <div>
       <Header/>
-      <Painting size="30x20cm" name="Stina och Lisa" sold={true} price="1799kr/st" image="tavla.jpg"/>
+      <Painting size="30x20cm" name="Stina och Lisa" price="1799kr/st" image="tavla.jpg"/>
       <Painting size="30x50cm" name="Berit och Hjärtrud" price="2399kr" image="tavla2.jpg" />
-      <Painting size="25x25cm" name="De tre mittbenorna" price="1200kr/st" image="tavla3.jpg" />
+      <Painting size="30x20cm" name="Elvira Blåtira" price="1999kr" image="tavla-orange.jpg" />
+      <Painting size="30x20cm" name="Elvira Blåtira" price="1999kr" image="tavla-sophie.jpg" />
+      <Painting size="30x20cm" name="Elvira Blåtira" price="1999kr" image="tavla-mittbenan.jpg" />
+      <Painting size="30x20cm" name="Elvira Blåtira" price="1999kr" image="tavla-sne.jpg" />
       <Painting size="30x20cm" name="Elvira Blåtira" price="1999kr" image="tavla4.jpg" />
     </div>
     
